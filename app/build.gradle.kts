e1c4f3dc97fa9bce2f22dc125f3975c1cc7aec0d
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-07-17 21:49:00 +0300

    Bump Crownfall world conquest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mehmetdem.crownfall"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
     }
 
     buildTypes {
